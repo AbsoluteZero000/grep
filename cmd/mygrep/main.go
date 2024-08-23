fced2b8a4af401b1fe031cc210315d5cc56852d7
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-23 11:59:20 +0300

    feat: added character match

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	// Uncomment this to pass the first stage
-	// "bytes"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
 	"unicode/utf8"
 )
 
-// Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
 		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
-		os.Exit(2) // 1 means no lines were selected, >1 means error
+		os.Exit(2)
 	}
 
 	pattern := os.Args[2]
 
-	line, err := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
+	line, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
 		os.Exit(2)
@@ -41,14 +39,7 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	if utf8.RuneCountInString(pattern) != 1 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
-
-	var ok bool
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this to pass the first stage
-	// ok = bytes.ContainsAny(line, pattern)
+	ok := bytes.ContainsAny(line, pattern)
 
 	return ok, nil
 }
