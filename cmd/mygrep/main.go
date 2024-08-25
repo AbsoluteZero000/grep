71f96216a447869184be54f630b96576d36d7cf6
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-25 19:17:16 +0300

    feat: added support for | operator

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -21,7 +21,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
 		os.Exit(2)
 	}
-
 	ok, err := matchLine(string(line), pattern)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
@@ -48,36 +47,81 @@ func matchLine(line string, pattern string) (bool, error) {
     if utf8.RuneCountInString(pattern) == 0 {
         return false, fmt.Errorf("unsupported pattern: %q", pattern)
     }
-    endAnchor := false
-    length := len(line)
-    if pattern[0] == '^' {
-        length = 1
-        pattern = pattern[1:]
-    }
-    if pattern[len(pattern)-1] == '$' {
-        pattern = pattern[:len(pattern)-1]
-        endAnchor = true
-    }
-    for j := 0; j < length; j++ {
-        i := 0
-        matchStart := j
-        for j < len(line) && i < len(pattern) {
-            if pattern[i] == '\\' {
-                if i+1 < len(pattern) && pattern[i+1] == '\\' {
-                    i++
-                }
-                if i+1 < len(pattern) {
-                    if pattern[i+1] == 'd' || pattern[i+1] == 'w' {
-                        if !charMatches(pattern[i:i+2], line[j]) {
-                            break
-                        }
-                        j++
-                        i += 2
-                    } else {
-                        return false, fmt.Errorf("invalid pattern: %q", pattern)
-                    }
-                }
-            } else if pattern[i] == '[' {
+	if strings.Contains(pattern, "|") {
+		i := 0
+		for ; i < len(pattern); i++ {
+			if pattern[i] == '(' {
+				break
+			}
+		}
+		if i == len(pattern) {
+			patterns := strings.Split(pattern, "|")
+			for _, p := range patterns {
+				ok, err := match(line, p)
+				if err != nil {
+					return false, err
+				}
+				if ok {
+					return true, nil
+				}
+			}
+			return match(line, pattern)
+		}
+		j := i+1
+		for ; j < len(pattern); j++ {
+			if pattern[j] == ')' {
+				break
+			}
+		}
+		patterns := strings.Split(pattern[i+1:j], "|")
+		for _, p := range patterns {
+			p = pattern[:i] + p + pattern[j+1:]
+			fmt.Println(p)
+			ok, err := match(line, p)
+			if err != nil {
+				return false, err
+			}
+			if ok {
+				return true, nil
+			}
+		}
+		return false, nil
+	}else {
+		return match(line, pattern)
+	}
+}
+
+func match(line string, pattern string) (bool, error) {
+	endAnchor := false
+	length := len(line)
+	if pattern[0] == '^' {
+		length = 1
+		pattern = pattern[1:]
+	}
+	if pattern[len(pattern)-1] == '$' {
+		pattern = pattern[:len(pattern)-1]
+		endAnchor = true
+	}
+	for j := 0; j < length; j++ {
+		i := 0
+		matchStart := j
+		for j < len(line) && i < len(pattern) {
+			if pattern[i] == '\\' {
+				if i+1 < len(pattern) && pattern[i+1] == '\\' {
+					i++
+				}
+				if i+1 < len(pattern) {
+					if pattern[i+1] == 'd' || pattern[i+1] == 'w' {
+						if !charMatches(pattern[i:i+2], line[j]) {
+							break
+						}
+						j++
+						i += 2
+					} else {
+						return false, fmt.Errorf("invalid pattern: %q", pattern)
+					}
+				}
+			} else if pattern[i] == '[' {
 				k := i
 				for k < len(pattern) && pattern[k] != ']' {
 					k++
@@ -100,42 +144,43 @@ func matchLine(line string, pattern string) (bool, error) {
 				}
 				i = k + 1
 				j++
-            } else if i+1 < len(pattern) && (pattern[i+1] == '+' || pattern[i+1] == '*' || pattern[i+1] == '?') {
-                operator := pattern[i+1]
-                char := pattern[i]
-                i += 2
-                count := 0
-                for j < len(line) && charMatches(string(char), line[j]) {
-                    j++
-                    count++
-                }
-                if operator == '+' && count == 0 {
-                    break
-                }
-                if operator == '?' && count > 1 {
-                    j -= count - 1
-                }
-            } else if pattern[i] == '.' {
+			} else if i+1 < len(pattern) && (pattern[i+1] == '+' || pattern[i+1] == '*' || pattern[i+1] == '?') {
+				operator := pattern[i+1]
+				char := pattern[i]
+				i += 2
+				count := 0
+				for j < len(line) && charMatches(string(char), line[j]) {
+					j++
+					count++
+				}
+				if operator == '+' && count == 0 {
+					break
+				}
+				if operator == '?' && count > 1 {
+					j -= count - 1
+				}
+			} else if pattern[i] == '.' {
 				j++
 				i++
 			}else {
-                if pattern[i] != line[j] {
-                    break
-                }
-                i++
-                j++
-            }
-        }
-        if i == len(pattern) {
-            if endAnchor {
-                if j == len(line) {
-                    return true, nil
-                }
-            } else {
-                return true, nil
-            }
-        }
-        j = matchStart
-    }
-    return false, nil
+				if pattern[i] != line[j] {
+					break
+				}
+				i++
+				j++
+			}
+		}
+		if i == len(pattern) {
+			if endAnchor {
+				if j == len(line) {
+					return true, nil
+				}
+			} else {
+				return true, nil
+			}
+		}
+		j = matchStart
+	}
+	return false, nil
+
 }
